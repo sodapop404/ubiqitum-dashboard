facf58f622a924342ee807d402c80272c490d92e
sodapop <68544142+sodapop404@users.noreply.github.com>
2026-02-01 10:39:09 +1100

    Update state.js

--- src/core/state.js
+++ src/core/state.js
@@ -1,1 +1,17 @@
+// state.js
+
+let dashboardState = null;
+
+function setState(data) {
+  dashboardState = data;
+}
+
+function getState() {
+  return dashboardState;
+}
+
+export const state = {
+  setState,
+  getState
+};
 
