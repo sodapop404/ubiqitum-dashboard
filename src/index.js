51e8cb29cdf87e5c9af3cae49f859f0cee12589f
sodapop <68544142+sodapop404@users.noreply.github.com>
2026-02-01 21:44:41 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { fetchDashboardData } from "./core/apiClient";
-import { initLineMetrics } from "./widgets/lineMetrics";
-import { keyBrandMetric_ringChart } from './widgets/keyBrandMetric_ringChart.js'
+import KeyBrandMetricRingChart from './widgets/keyBrandMetric_ringChart.js';
 import './styles/charts.css';
 
 window.keyBrandMetric_ringChart = keyBrandMetric_ringChart;
@@ -11,7 +10,6 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const data = await fetchDashboardData(url);
 
-  initLineMetrics(data);
   initRingMetrics(data);
 });
 
