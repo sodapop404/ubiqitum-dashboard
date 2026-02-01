cc928f97952789b6d2540384ca17f565cc6f8064
sodapop <68544142+sodapop404@users.noreply.github.com>
2026-02-01 20:44:51 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,20 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      name: 'UbiqitumDashboard',
+      fileName: () => 'ubiqitum-dashboard.bundle.js'
+    },
+    rollupOptions: {
+      external: ['chart.js'],
+      output: {
+        globals: {
+          'chart.js': 'Chart'
+        }
+      }
+    }
+  }
+});
 
