b65c156fa923cb24fe2eb89ad6b994ef47b3cc55
sodapop <68544142+sodapop404@users.noreply.github.com>
2026-02-01 20:52:44 +1100

    Update ringChart.entry.js

--- src/entries/ringChart.entry.js
+++ src/entries/ringChart.entry.js
@@ -1,5 +1,5 @@
-import { keyBrandMetric_ringChart } from "../widgets/keyBrandMetric_ringChart";
+import "../styles/charts.css";
+import { keyBrandMetric_ringChart } from "../widgets/keyBrandMetric_ringChart.js";
 
 // expose globally for Webflow
 window.keyBrandMetric_ringChart = keyBrandMetric_ringChart;
-
